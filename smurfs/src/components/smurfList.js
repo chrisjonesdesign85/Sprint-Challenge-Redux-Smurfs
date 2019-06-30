d50f9f6faf8fc53558341642e646929f87d6e06c
Chris Jones <chrisjonesdesign85@gmail.com>
2019-06-30 18:56:14 -0400

    created a smurfList and SmurfForm components

---
+++ smurfs/src/components/smurfList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Smurf from './Smurf';
+
+const SmurfList = props => {
+    return (
+        <div>
+            <h2>The Smurf Village</h2>
+            <div className='smurf-list-wrapper'>
+                {props.smurfs.map((smurf, index) => <Smurf key={index} smurf={smurf} deleteSmurf={props.deleteSmurf} />)}
+            </div>
+        </div>
+    )
+}
+
+export default SmurfList;
