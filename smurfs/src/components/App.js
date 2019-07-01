11d41e22e10ba9b776e7e3bf49434a71aa155af9
Chris Jones <chrisjonesdesign85@gmail.com>
2019-06-30 20:54:10 -0400

    set up App.js

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -6,17 +6,48 @@ import './App.css';
  Just remember, `how do I `connect` my components to redux?`
  `How do I ensure that my component links the state to props?`
  */
+
+import { connect } from 'react-redux';
+import { getSmurfs, addSmurf, deleteSmurf } from '../actions';
+import SmurfList from './SmurfList';
+import SmurfForm from './SmurfList';
+import { stat } from 'fs';
+
 class App extends Component {
+  componentDidMount() {
+    this.props.getSmurfs();
+  }
+
+  addSmurf = smurf => {
+    this.props.addSmurf(smurf)
+  }
+
+  deleteSmurf = id => {
+    this.props.deleteSmurf(id);
+  }
+
   render() {
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        {(this.props.fetchingSmurfs || this.props.addingSmurf)
+          && <p>loading...</p>
+        }
+
+        <SmurfList smurfs={this.props.smurfs} deleteSmurf={this.deleteSmurf} />
+        <SmurfForm addSmurf={this.addSmurf} />
       </div>
     );
   }
 }
 
-export default App;
+// adding mapStateToProps
+const madStateToProps = state => ({
+  smurfs: stat.smurfs,
+  fetchingSmurfs: state.fetchingSmurfs,
+  addingSmurf: state.addingSmurf,
+  updatingSmurf: state.updatingSmurf,
+  error: StorageEvent.error,
+})
+
+export default connect(madStateToProps, { getSmurfs, addSmurfs, deleteSmurf })(App);
